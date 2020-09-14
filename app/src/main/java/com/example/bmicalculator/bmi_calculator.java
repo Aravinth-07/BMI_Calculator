ea85e5f191f06caff0ad1ddbea59cd0b926874d5
Aravinth-07 <strong7aravinth2000@gmail.com>
2020-09-14 10:01:09 +0530

    bmi portion completed

--- app/src/main/java/com/example/bmicalculator/bmi_calculator.java
+++ app/src/main/java/com/example/bmicalculator/bmi_calculator.java
@@ -6,19 +6,51 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class bmi_calculator extends AppCompatActivity {
-
+    EditText e1,e2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi_calculator);
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+         e1=(EditText)findViewById(R.id.et1);
+         e2=(EditText)findViewById(R.id.et2);
+
     }
 
     public void btnonclick(View view) {
-        Intent i=new Intent(this,bmi_result.class);
-        startActivity(i);
+        try {
+            String height = e1.getText().toString();
+            String weight = e2.getText().toString();
+            double height1 = Double.parseDouble(height);
+            double weight1 = Double.parseDouble(weight);
+            if (height.length() == 0) {
+                height1 = 0;
+                e1.setError("Height cannot be empty");
+            } else if (weight.length() == 0) {
+                weight1 = 0;
+                e2.setError("Weight cannot be empty");
+            } else if (weight.length() != 0 && height.length() != 0) {
+                if (height1 <= 30 || height1 >= 300) {
+                    e1.setError("Enter correct height value");
+                } else if (weight1 >= 1000) {
+                    e2.setError("Enter correct weight value");
+                } else {
+                    Intent i = new Intent(this, bmi_result.class);
+                    i.putExtra("height", height);
+                    i.putExtra("weight", weight);
+                    startActivity(i);
+                }
+            }
+        }
+        catch (NumberFormatException e)
+        {
+            e1.setError("Height cannot be empty");
+            e2.setError("Weight cannot be empty");
+        }
+
     }
 }
--- app/src/main/java/com/example/bmicalculator/bmi_result.java
+++ app/src/main/java/com/example/bmicalculator/bmi_result.java
@@ -3,9 +3,13 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class bmi_result extends AppCompatActivity {
+    TextView t1,t2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,44 @@ public class bmi_result extends AppCompatActivity {
         setContentView(R.layout.activity_bmi_result);
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        t1=(TextView)findViewById(R.id.bmi_value);
+        t2=(TextView)findViewById(R.id.bmi_result);
+        Bundle b =getIntent().getExtras();
+        String Height=b.getString("height");
+        String Weight=b.getString("weight");
+        double height=Double.parseDouble(Height);
+        double weight=Double.parseDouble(Weight);
+        double h1=height/100;
+        double H1=Math.pow(h1,2);
+        double result1=(weight/H1);
+        String result2=String.format("%.2f",result1);
+        double result=Double.parseDouble(result2);
+
+        if(result<=18.5)
+        {
+            t1.setText("Your BMI is "+result+" ");
+            t2.setText("Result : Underweight");
+        }
+        else if(result>18.5&&result<=24.9)
+        {
+            t1.setText("Your BMI is "+result+" ");
+            t2.setText("Result : Normal weight");
+        }
+        else if(result>24.9&&result<29.9)
+        {
+            t1.setText("Your BMI is "+result+" ");
+            t2.setText("Result : Overweight");
+        }
+        else if(result>=29.9)
+        {
+            t1.setText("Your BMI is "+result+" ");
+            t2.setText("Result : Obese");
+        }
+
+    }
+
+    public void btnnext(View view) {
+        Intent i=new Intent(this,bmi_calculator.class);
+        startActivity(i);
     }
 }
